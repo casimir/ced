141c536fc145183222dc0fb23ef24599921e4f8c
Martin Chaine <martin@casimir.me>
2021-01-03 21:24:19 +0100

    rework ModalHandler to allow nested modes

--- core/scripts/keys.lua
+++ core/scripts/keys.lua
@@ -3,71 +3,139 @@ local M = {}
 local ModalHandler = {}
 ModalHandler.__index = ModalHandler
 
-ModalHandler.modes = {normal = "N", insertion = "I"}
-
 setmetatable(ModalHandler, {__call = function(cls, ...) return cls.new(...) end})
+
+local function make_goto_mappings(extend)
+    return {
+        ["h"] = {
+            fn = function(mh)
+                mh:exit_mode()
+                return editor:move_to_line_begin(mh.client_id, extend)
+            end
+        },
+        ["l"] = {
+            fn = function(mh)
+                mh:exit_mode()
+                return editor:move_to_line_end(mh.client_id, extend)
+            end
+        },
+        default = function(mh, key) mh:exit_mode() end
+    }
+end
+
+ModalHandler.modes = {
+    normal = {
+        name = "N",
+        title = "",
+        mappings = {
+            ["i"] = {fn = function(mh) mh:enter_mode("insertion") end},
+            ["g"] = {fn = function(mh) mh:enter_mode("moveto") end},
+            ["s-g"] = {fn = function(mh) mh:enter_mode("extendto") end},
+            ["h"] = {
+                fn = function(mh)
+                    return editor:move_left(mh.client_id, false)
+                end
+            },
+            ["j"] = {
+                fn = function(mh)
+                    return editor:move_down(mh.client_id, false)
+                end
+            },
+            ["k"] = {
+                fn = function(mh)
+                    return editor:move_up(mh.client_id, false)
+                end
+            },
+            ["l"] = {
+                fn = function(mh)
+                    return editor:move_right(mh.client_id, false)
+                end
+            },
+            ["s-h"] = {
+                fn = function(mh)
+                    return editor:move_left(mh.client_id, true)
+                end
+            },
+            ["s-j"] = {
+                fn = function(mh)
+                    return editor:move_down(mh.client_id, true)
+                end
+            },
+            ["s-k"] = {
+                fn = function(mh)
+                    return editor:move_up(mh.client_id, true)
+                end
+            },
+            ["s-l"] = {
+                fn = function(mh)
+                    return editor:move_right(mh.client_id, true)
+                end
+            },
+            ["m"] = {
+                fn = function(mh)
+                    editor:message(mh.client_id, "hello!")
+                end
+            },
+            ["e"] = {fn = function(mh)
+                editor:error(mh.client_id, "oops!")
+            end}
+        }
+    },
+    insertion = {
+        name = "I",
+        title = "",
+        mappings = {["esc"] = {fn = function(mh) mh:exit_mode() end}}
+    },
+    moveto = {name = "g", title = "", mappings = make_goto_mappings(false)},
+    extendto = {name = "G", title = "", mappings = make_goto_mappings(true)}
+}
 
 function ModalHandler.new(client_id)
     local self = setmetatable({}, ModalHandler)
     self.client_id = client_id
-    self.mode = ModalHandler.modes.normal
+    self.mode_stack = {"normal"}
     self.redraw_status = false
     return self
 end
+
+function ModalHandler:enter_mode(mode)
+    table.insert(self.mode_stack, mode)
+    self.redraw_status = true
+end
+
+function ModalHandler:exit_mode()
+    table.remove(self.mode_stack)
+    self.redraw_status = true
+end
+
+function ModalHandler:curmode() return self.mode_stack[#self.mode_stack] end
 
 function ModalHandler:set_status(key)
     local status_line = editor.clients[self.client_id].status_line
     if not status_line.keys then status_line.keys = {index = 80} end
     if not status_line.mode then status_line.mode = {index = 90} end
     status_line.keys.text = key and key.display or ""
-    status_line.mode.text = self.mode
+    status_line.mode.text = self.modes[self:curmode()].name
     self.redraw_status = true
 end
 
-function ModalHandler:handle_normal(key)
-    if key.value == "i" then
-        self.mode = ModalHandler.modes.insertion
-        self.redraw_status = true
-    elseif key.value == "h" then
-        self.redraw_status = editor:move_left(self.client_id, false)
-    elseif key.value == "j" then
-        self.redraw_status = editor:move_down(self.client_id, false)
-    elseif key.value == "k" then
-        self.redraw_status = editor:move_up(self.client_id, false)
-    elseif key.value == "l" then
-        self.redraw_status = editor:move_right(self.client_id, false)
-    elseif key.value == "H" then
-        self.redraw_status = editor:move_left(self.client_id, true)
-    elseif key.value == "J" then
-        self.redraw_status = editor:move_down(self.client_id, true)
-    elseif key.value == "K" then
-        self.redraw_status = editor:move_up(self.client_id, true)
-    elseif key.value == "L" then
-        self.redraw_status = editor:move_right(self.client_id, true)
-    elseif key.value == "m" then
-        editor:message(self.client_id, "hello!")
-    elseif key.value == "e" then
-        editor:error(self.client_id, "oops!")
-    end
-end
-
-function ModalHandler:handle_insertion(key)
-    if key.value == "esc" then
-        self.mode = ModalHandler.modes.normal
-        self.redraw_status = true
-    end
-end
-
 function ModalHandler:handle(key)
-    if self.mode == ModalHandler.modes.normal then
-        self:handle_normal(key)
-    elseif self.mode == ModalHandler.modes.insertion then
-        self:handle_insertion(key)
+    local mode = self.modes[self:curmode()]
+    local mapping = mode.mappings[key.display]
+    local handled = false
+    if mapping then
+        mapping.fn(self)
+        handled = true
+    elseif mode.mappings.default then
+        mode.mappings.default(self, key)
+        handled = true
     end
-    self:set_status(key)
-    local redraw_status = self.redraw_status
+
+    if handled then self:set_status(key) end
+
+    local ret = {redraw_status = self.redraw_status}
     self.redraw_status = false
-    return {redraw_status = redraw_status}
+    return ret
 end
 
 M.ModalHandler = ModalHandler
